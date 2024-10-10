a4a7f429fe7a747f5dc7548b7bd2e14a2b20526c
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-10-10 17:31:27 +0530

    Updated

--- components/auth-form.js
+++ components/auth-form.js
@@ -1,8 +1,12 @@
+'use client';
 import Link from 'next/link';
+import {useFormState} from 'react-dom';
+import { auth} from '@/actions/auth-actions';
 
-export default function AuthForm() {
+export default function AuthForm({mode}) {
+  const [formState, formAction]= useFormState(auth.bind(null,mode), {});
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -14,13 +18,26 @@ export default function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+
+      {formState.errors && (<ul id="form-errors">
+        {Object.keys(formState.errors).map((error) =>(
+          <li key={error}>{formState.errors[error]}</li>
+        ))}
+      </ul>
+    )}
+
       <p>
         <button type="submit">
-          Create Account
+          {mode==='login'? 'Login': 'Create Account'}
         </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode==='login' && (
+          <Link href="/?mode=signup">Create an account.</Link>
+          )}
+        {mode==='signup' && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
